67d48e8f67ae4b1aa70921c563c651abb7791ae6
Titas Valantinas <novatitas366@gmail.com>
2025-02-07 11:17:37 +0200

    added the compress method

--- Main.java
+++ Main.java
@@ -1,13 +1,29 @@
 public class Main {
 
-    String compress(String input){
+    public static String compress(String input){
+        if(input.isEmpty()){
+            return "";
+        }
         String Output = "";
-        
-
+        Integer charnum = 0;
+        char presentSymbol;
+        char pastSymbol=' ';
+        for(int i = 0; i< input.length(); i++){
+            presentSymbol = input.charAt(i);
+            if(presentSymbol != pastSymbol && pastSymbol != ' '){
+                Output+=pastSymbol+charnum.toString();
+                charnum=0;
+            }
+            charnum++;
+            pastSymbol = presentSymbol;
+        }
+        Output += pastSymbol + charnum.toString();
         return Output;
     }
 
+    
+
     public static void main(String[] args) {
-        
+        System.out.println(compress(""));
     }
 }

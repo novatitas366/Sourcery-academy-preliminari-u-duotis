0e3beb5e5a78a5911870e3d6954a389bb912745b
Titas Valantinas <novatitas366@gmail.com>
2025-02-07 12:12:03 +0200

    completed decompress method

--- Main.java
+++ Main.java
@@ -4,26 +4,38 @@ public class Main {
         if(input.isEmpty()){
             return "";
         }
-        String Output = "";
+        String output = "";
         Integer charnum = 0;
         char presentSymbol;
         char pastSymbol=' ';
         for(int i = 0; i< input.length(); i++){
             presentSymbol = input.charAt(i);
             if(presentSymbol != pastSymbol && pastSymbol != ' '){
-                Output+=pastSymbol+charnum.toString();
+                output+=pastSymbol+charnum.toString();
                 charnum=0;
             }
             charnum++;
             pastSymbol = presentSymbol;
         }
-        Output += pastSymbol + charnum.toString();
-        return Output;
+        output += pastSymbol + charnum.toString();
+        return output;
     }
 
-    
+    public static String decompress(String input){
+        String output = "";
+        if(input.isEmpty()){
+            return "";
+        }
+        for(int i = 0; i<input.length()-1; i+=2){
+            int j = i+1;
+            String symb = Character.toString(input.charAt(i)).repeat(Integer.parseInt(Character.toString(input.charAt(j))));
+            output += symb;
+        }
+        return output;
+    }
 
     public static void main(String[] args) {
         System.out.println(compress(""));
+        System.out.println(decompress("a11b3"));
     }
 }
